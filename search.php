819a07a15e4237aceb35b3739311261e80112d60
Rory Bolton <boltonr3@uni.coventry.ac.uk>
2016-04-22 18:05:56 +0100

    Update search.php

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta  http-equiv="Content-Type" content="text/html;  charset=iso-8859-1">
-	<title>Search  Contacts</title>
+	<title>Search Standards</title>
 </head>
 <p><body>
 	<h3>Search Database</h3>
